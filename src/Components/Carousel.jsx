1913c642dea77beeaf6e2fbe425c0f770f36ad85
Kritika745 <yadavkritika745@gmail.com>
2024-02-26 19:42:46 +0530

    carousel

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Navigation, Pagination,Mousewheel} from 'swiper/modules';
-import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import './Carousel.css'
+import { Link } from 'react-router-dom';
+import { ArrowRightCircle } from 'lucide-react';
 
 const Carousel = () => {
   return (
@@ -16,8 +17,6 @@ const Carousel = () => {
         slidesPerView={1}
         centeredSlides={true}
         loop={true}
-        
-  
         cssMode={true}
         sticky={false}
         freeMode={{ enable: true }}
@@ -25,18 +24,24 @@ const Carousel = () => {
           mousewheel: true,
         }}
         navigation={true}
-        breakpoints={{
-          0: {
-            slidesPerView: 1,
-          },
-          640: {
-            slidesPerView: 1,
-          },
-        }}
         modules={[ Pagination, Navigation, Mousewheel]}
       >
         <SwiperSlide>
-            <div className='slide1 flex w-full h-[75vh] justify-center items-center'>
+            <div className='slide1 flex w-full h-[60vh] md:h-[75vh] justify-center items-center'>
+                 <div className='bg-black bg-opacity-35 w-full h-full flex flex-col justify-center items-center '>
+                    <div className='flex flex-col items-center'>
+                    <h1 className='text-white text-5xl font-bold'>EMPIRE ENGINEERING SERVICES</h1>
+                    <h2 className='text-white font-semibold text-3xl '>Driven by excellence,built on culture</h2>
+                    </div>
+                    <div>
+                      <Link to='/#about'><button className='bg-[#00cc99] mt-4 w-30 px-3 py-2 rounded-md font-bold flex text-[1.15rem]'> Learn More <ArrowRightCircle className='ml-2 mt-[1.5px]' size={25}/></button></Link>
+                    </div>
+                    
+                    </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slide2 flex w-full h-[60vh] md:h-[75vh] justify-center items-center'>
                  <div className='bg-black bg-opacity-35 w-full h-full flex justify-center items-center '>
                     <div>
                     <h1 className='text-white text-4xl'>EMPIRE ENGINEERING SERVICES</h1>
@@ -44,32 +49,78 @@ const Carousel = () => {
                     
                     </div>
             </div>
-          
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className=" w-full " src="../../Public/slide2.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className=" w-full  " src="../../Public/slide3.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className=" w-full " src="../../Public/slide4.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className=" w-full" src="../../Public/slide5.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className=" w-full" src="../../Public/slide6.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className=" w-full" src="../../Public/slide7.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className=" w-full" src="../../Public/slide8.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className=" w-full" src="../../Public/slide9.jpg" alt="" />
-        </SwiperSlide>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slide3 flex w-full h-[60vh] md:h-[75vh] justify-center items-center'>
+                 <div className='bg-black bg-opacity-35 w-full h-full flex justify-center items-center '>
+                    <div>
+                    <h1 className='text-white text-4xl'>EMPIRE ENGINEERING SERVICES</h1>
+                    </div>
+                    
+                    </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slide4 flex w-full h-[60vh] md:h-[75vh] justify-center items-center'>
+                 <div className='bg-black bg-opacity-35 w-full h-full flex justify-center items-center '>
+                    <div>
+                    <h1 className='text-white text-4xl'>EMPIRE ENGINEERING SERVICES</h1>
+                    </div>
+                    
+                    </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slide5 flex w-full h-[60vh] md:h-[75vh] justify-center items-center'>
+                 <div className='bg-black bg-opacity-35 w-full h-full flex justify-center items-center '>
+                    <div>
+                    <h1 className='text-white text-4xl'>EMPIRE ENGINEERING SERVICES</h1>
+                    </div>
+                    
+                    </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slide6 flex w-full h-[60vh] md:h-[75vh] justify-center items-center'>
+                 <div className='bg-black bg-opacity-35 w-full h-full flex justify-center items-center '>
+                    <div>
+                    <h1 className='text-white text-4xl'>EMPIRE ENGINEERING SERVICES</h1>
+                    </div>
+                    
+                    </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slide7 flex w-full h-[60vh] md:h-[75vh] justify-center items-center'>
+                 <div className='bg-black bg-opacity-35 w-full h-full flex justify-center items-center '>
+                    <div>
+                    <h1 className='text-white text-4xl'>EMPIRE ENGINEERING SERVICES</h1>
+                    </div>
+                    
+                    </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slide8 flex w-full h-[60vh] md:h-[75vh] justify-center items-center'>
+                 <div className='bg-black bg-opacity-35 w-full h-full flex justify-center items-center '>
+                    <div>
+                    <h1 className='text-white text-4xl'>EMPIRE ENGINEERING SERVICES</h1>
+                    </div>
+                    
+                    </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slide9 flex w-full h-[60vh] md:h-[75vh] justify-center items-center'>
+                 <div className='bg-black bg-opacity-35 w-full h-full flex justify-center items-center '>
+                    <div>
+                    <h1 className='text-white text-4xl'>EMPIRE ENGINEERING SERVICES</h1>
+                    </div>
+                    
+                    </div>
+            </div>
+          </SwiperSlide>
+        
       </Swiper>
       </div>
     </>
