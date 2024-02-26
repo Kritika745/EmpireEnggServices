912c1bfa00b8461105c6cca01af2501de9948b35
Kritika745 <yadavkritika745@gmail.com>
2024-02-26 17:45:43 +0530

    nav changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,6 +12,14 @@ const Navbar=()=> {
     setIsMenuOpen(!isMenuOpen)
   }
 
+  const [isMenuOpen1, setIsMenuOpen1] = React.useState(false)
+  const location1 = useLocation();
+
+  const toggleMenu1 = () => {
+    setIsMenuOpen1(!isMenuOpen1)
+  }
+  
+
   const currentPathname = location.pathname;
   console.log(currentPathname)
  
@@ -25,7 +33,34 @@ const Navbar=()=> {
           <ul className="flex gap-8">
             <Link to='/'><li className={`text-base hover:text-[#00cc99] font-bold ${currentPathname === '/' ? 'border-b-2 border-[#00cc99] text-[#00cc99]' : 'border-none text-white'}`}>Home</li></Link>           
              <Link to='/#about'><li className={`text-base hover:text-[#00cc99] font-bold ${currentPathname === '/#about' ? 'border-b-2 border-[#00cc99] text-[#00cc99]' : 'border-none text-white'} `}>About Us</li></Link>
-            <Link to='/#services'><li className={`text-base  hover:text-[#00cc99] font-bold ${currentPathname === '/#services' ? 'border-b-2 border-[#00cc99] text-[#00cc99]' : 'border-none text-white'} `}>Services</li></Link>
+             <li className="relative z-50 ">
+              <span className={`text-base hover:text-[#00cc99] font-bold cursor-pointer ${currentPathname.includes('/services') ? 'border-b-2 border-[#00cc99] text-[#00cc99]' : 'border-none text-white'}`} onClick={toggleMenu1}>Services</span>
+              {isMenuOpen1 && (
+                <ul className="blue p-1 w-[19vw] ring-1 ring-white ring-opacity-20   absolute mt-2  text-white font-bold rounded-lg shadow-lg">
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/FieldServices'>Field Services</Link>
+                  </li>
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/ConstructionServices'>Construction Services</Link>
+                  </li>
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/RadioAccessNetworkServices'>Radio Access Network Services</Link>
+                  </li>
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/OutsidePlantServices'>Outside Plant Engineering Services</Link>
+                  </li>
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/DataCenterServices'>Data Center Services</Link>
+                  </li>
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/NOCServices'>NOC Services</Link>
+                  </li>
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/ProfessionalServices'>Professional Services</Link>
+                  </li>
+                </ul>
+              )}
+            </li>            
             <Link to='/Careers'><li className={`text-base hover:text-[#00cc99] font-bold ${currentPathname === '/Careers' ? 'border-b-2 border-[#00cc99] text-[#00cc99]' : 'border-none text-white'} `}>Careers</li></Link>
             <Link to='/Contact'><li className={`text-base hover:text-[#00cc99] font-bold ${currentPathname === '/Contact' ? 'border-b-2 border-[#00cc99] text-[#00cc99]' : 'border-none text-white'}`}>Contact Us</li></Link>   
           </ul>
@@ -41,7 +76,7 @@ const Navbar=()=> {
                   <div className=" ">
                   <img className='w-[100px] ' src={logo} alt="" />
                   </div>
-                  <div >
+                  <div>
                     <button
                       type="button"
                       onClick={toggleMenu}
@@ -54,10 +89,37 @@ const Navbar=()=> {
                 <div className="mt-6">
                   <nav className="grid gap-2">
                      <Link to='/'><li onClick={()=>setIsMenuOpen(false)} className={`text-base hover:text-[#00cc99] font-bold  ${currentPathname === '/' ? 'text-[#00cc99]' : 'border-none text-white'} `}>Home</li></Link>
-                     <Link to='/#about'><li  onClick={()=>setIsMenuOpen(false)} className={`text-base hover:text-[#00cc99] font-bold ${currentPathname === '/#services' ? ' text-[#00cc99]' : 'border-none text-white'} `}>About Us</li></Link>
-                     <Link to='/#services'><li  onClick={()=>setIsMenuOpen(false)} className={`text-base  hover:text-[#00cc99] font-bold ${currentPathname === '/#about' ? ' text-[#00cc99]' : 'border-none text-white'} `}>Services</li></Link>
-                     <Link to='/Careers'><li  onClick={()=>setIsMenuOpen(false)} className={`text-base hover:text-[#00cc99] font-bold ${currentPathname === '/Careers' ? ' text-[#00cc99]' : 'border-none text-white'} `}>Careers</li></Link>
-                     <Link to='/Contact'><li onClick={()=>setIsMenuOpen(false)} className={`text-base hover:text-[#00cc99] font-bold ${currentPathname === '/Contact' ? ' text-[#00cc99]' : 'border-none text-white'}`}>Contact Us</li></Link> 
+                     <Link to='/#about'><li  onClick={()=>setIsMenuOpen(false)} className={`text-base hover:text-[#00cc99] font-bold ${currentPathname === '/#about' ? ' text-[#00cc99]' : 'border-none text-white'} `}>About Us</li></Link>
+                     <li   className={`relative z-50 text-base hover:text-[#00cc99] font-bold cursor-pointer ${currentPathname.includes('/services') ? 'border-b-2 border-[#00cc99] text-[#00cc99]' : 'border-none text-white'}`} >
+              <span  onClick={toggleMenu1}>Services</span>
+              {isMenuOpen1 && (
+                <ul className="blue p-1 w-[19vw] ring-1 ring-white ring-opacity-20   absolute mt-2  text-white font-bold rounded-lg shadow-lg">
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/FieldServices'>Field Services</Link>
+                  </li>
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/ConstructionServices'>Construction Services</Link>
+                  </li>
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/RadioAccessNetworkServices'>Radio Access Network Services</Link>
+                  </li>
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/OutsidePlantServices'>Outside Plant Engineering Services</Link>
+                  </li>
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/DataCenterServices'>Data Center Services</Link>
+                  </li>
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/NOCServices'>NOC Services</Link>
+                  </li>
+                  <li className="py-1 px-2 hover:text-[#00cc99]">
+                    <Link to='/ProfessionalServices'>Professional Services</Link>
+                  </li>
+                </ul>
+              )}
+            </li>                          
+            <Link to='/Careers'><li  onClick={()=>setIsMenuOpen(false)} className={`text-base hover:text-[#00cc99] font-bold ${currentPathname === '/Careers' ? ' text-[#00cc99]' : 'border-none text-white'} `}>Careers</li></Link>
+            <Link to='/Contact'><li onClick={()=>setIsMenuOpen(false)} className={`text-base hover:text-[#00cc99] font-bold ${currentPathname === '/Contact' ? ' text-[#00cc99]' : 'border-none text-white'}`}>Contact Us</li></Link> 
                   </nav>
                 </div>
                
