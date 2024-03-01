6a04ca76565dfa8f8865b85c493164d234f47b18
utkarsharsh <harshupadhyay7786@gmail.com>
2024-03-01 23:08:46 +0530

    axios error may be ther

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
-import axios from 'axios'
+import axios from "axios"
 
 const Contact = () => {
   const [data,setdata]=useState({});
