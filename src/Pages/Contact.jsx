68874395420bd4760e1ea3ff97314f2c76372968
Kritika745 <yadavkritika745@gmail.com>
2024-03-05 00:18:49 +0530

    contact us changes

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -53,18 +53,18 @@ const Contact = () => {
 
 
 
-     <div className='flex flex-col content-center justify-center w-6/6 sm:w-4/6 m-8 rounded-lg bg-slate-600 sm:flex-row' >
-      <div className='flex  flex-col' >
-      <h3 className="text-3xl text-white font-bold  mt-5 mb-5 ml-4">Contact Us</h3>
-      <p className="text-1xl text-white font-normal mt-5 mb-4 ml-4">Fill up the form and our Team will get back to you within 24 hours.</p>
+     <div className='flex flex-col content-center justify-center w-full sm:w-4/6 m-8 rounded-lg bg-[#00cc99] bg-opacity-45 sm:flex-row' >
+      <div className='flex  flex-col text_blue ' >
+      <h3 className="text-3xl font-bold  mt-2 mb-5 ml-4 poppins pt-10">Contact Us</h3>
+      <p className="text-lg  mb-4 ml-4">Fill up the form and our Team will get back to you within 24 hours.</p>
       <div>
         <ul>
-          <li className='text-lg text-white mt-1 mb-3 ml-4'> <a rel="stylesheet" href="" >  +1 877-337-5520
+          <li className='text-lg  mt-1 mb-3 ml-4'> <a rel="stylesheet" href="" >  +1 877-337-5520
           
            </a> </li>
-           <li className='text-lg text-white mt-1 mb-3 ml-4'>    <a href="mailto:info@eesllc.com"><i class="fa-solid fa-envelopes-bulk me-2" aria-hidden="true"></i> info@eesllc.com</a>
+           <li className='text-lg mt-1 mb-3 ml-4'>    <a href="mailto:info@eesllc.com"><i class="fa-solid fa-envelopes-bulk me-2" aria-hidden="true"></i> info@eesllc.com</a>
          </li>
-          <li className='text-lg text-white mt-1 mb-3 ml-4'> <a target="_blank" href="https://goo.gl/maps/afZQE1Lp8MKo25Cy6" className='ml-0'><i class="fa-solid fa-location-dot me-3" aria-hidden="true"> !!</i> Corporate HQ <br/> 100 N Central Expy, Richardson, Texas 75080</a> </li>
+          <li className='text-lg mt-1 mb-3 ml-4'> <a target="_blank" href="https://goo.gl/maps/afZQE1Lp8MKo25Cy6" className='ml-0'><i class="fa-solid fa-location-dot me-3" aria-hidden="true"> !!</i> Corporate HQ <br/> 100 N Central Expy, Richardson, Texas 75080</a> </li>
         </ul>
       </div>
       
@@ -72,24 +72,24 @@ const Contact = () => {
 
 
       </div>
-      <div className='flex flex-col mt-8 mb-8 bg-slate-50 m-6 rounded-md' >
-      <div className='mb-4 mt-3 ml-5 mr-5'>
-                                <label for="fullname" class="form-label">Full Name</label>
-                                <input type="text" class="form-control" name="name" id="fullname" required="" className="border-lime-900 outline rounded-sm outline-slate-500 w-full" onChange={handlechanges}/>
+      <div className='flex flex-col mt-8 mb-8 blue py-5  rounded-md' >
+      <div className='mb-2 mt-2 ml-5 mr-5'>
+                                <label for="fullname" class="form-label text-white ">Full Name</label>
+                                <input type="text" class="form-control" name="name" id="fullname" required="" className="border-[1px] h-8 border-[#00cc99] rounded-sm  w-full" onChange={handlechanges}/>
                             </div>
-                            <div className='mb-4 mt-3 ml-5 mr-5'>
-                                <label for="fullname" class="form-label">Email address</label>
-                                <input type="text" class="form-control" name="email" id="fullname" required="" className="border-lime-900 outline rounded-sm outline-slate-500 w-full" onChange={handlechanges}/>
+                            <div className='mb-2 mt-2 ml-5 mr-5'>
+                                <label for="fullname" class="form-label text-white">Email address</label>
+                                <input type="text" class="form-control" name="email" id="fullname" required="" className="border-[1px] h-8 border-[#00cc99] rounded-sm  w-full" onChange={handlechanges}/>
                             </div>
-                            <div className='mb-4 mt-3 ml-5 mr-5'>
-                                <label for="fullname" class="form-label">Phone Number</label>
-                                <input type="text" class="form-control" name="phone" id="fullname" required="" className="border-lime-900 outline rounded-sm outline-slate-500 w-full" onChange={handlechanges}/>
+                            <div className='mb-2 mt-2 ml-5 mr-5'>
+                                <label for="fullname" class="form-label text-white">Phone Number</label>
+                                <input type="text" class="form-control" name="phone" id="fullname" required="" className="border-[1px] h-8 border-[#00cc99] rounded-sm  w-full" onChange={handlechanges}/>
                             </div>
-                            <div className='mb-4 mt-3 ml-5 mr-5'>
-                                <label for="fullname" class="form-label">Comment</label>
-                                <input type="textarea" class="form-control" name="comment" id="fullname" required="" className="border-lime-900 outline rounded-sm outline-slate-500 w-full" onChange={handlechanges}/>
+                            <div className='mb-2 mt-2 ml-5 mr-5'>
+                                <label for="fullname" class="form-label text-white">Comment</label>
+                                <input type="textarea" class="form-control" name="comment" id="fullname" required="" className="border-[1px] h-8 border-[#00cc99] rounded-sm  w-full" onChange={handlechanges}/>
                             </div>
-                            <button className='mb-4 mt-3 ml-5 mr-5 bg-emerald-600 w-1/3 text-white rounded-md h-10' onClick={handleclick}>
+                            <button className='mb-2 mt-2 ml-5 mr-5 bg-[#00cc99] hover:bg-emerald-600 w-1/3 font-semibold rounded-md h-10' onClick={handleclick}>
                               Submit
                             </button>
 
